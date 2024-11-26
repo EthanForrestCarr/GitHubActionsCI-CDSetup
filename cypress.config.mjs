3593d0801c12f05dff2574df21a6a7753ccecfe3
EthanForrestCarr <ethan@ethancarr.com>
2024-11-26 10:53:58 -0600

    deleted everything. reinstalled and rebuilt

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(_on, _config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
